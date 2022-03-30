14771f51534df7b10058b7a7096964c575eea299
uniArchitect <hancho93@gmail.com>
2022-03-29 21:04:25 -0400

    New functions for add, subtract, multiply, divide

---
+++ calculator-code.js
@@ -0,0 +1,21 @@
+
+
+function add(intOne, intTwo) {
+	let sum = intOne + intTwo;
+    return sum;
+};
+
+function subtract(intOne, intTwo) {
+	let difference = intOne - intTwo;
+    return difference;
+};
+
+function multiply(intOne, intTwo) {
+    let product = intOne * intTwo;
+    return product;
+}
+
+function divide(dividend, divisor) {
+    let quotient = dividend /= divisor;
+    return quotient;
+}
